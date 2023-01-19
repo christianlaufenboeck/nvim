bf32bfb9a4143564a9cbb6cae04dcd2d7287239a
Christian Laufenböck <christian@laufenboeck.com>
2023-01-19 13:50:19 +0100

    remove unnecessary quote

--- lua/christian/core/keymaps.lua
+++ lua/christian/core/keymaps.lua
@@ -17,7 +17,7 @@ keymap.set("i", "jk", "<ESC>")
 keymap.set("n", "<leader>nh", ":nohl<CR>")
 
 -- delete current selection without adding to registry
-keymap.set("v", "D", '"_d"')
+keymap.set("v", "D", '"_d')
 
 -- delete single character without copying into register
 keymap.set("n", "x", '"_x')
